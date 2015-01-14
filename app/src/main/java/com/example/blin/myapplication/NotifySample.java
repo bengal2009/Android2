3f241edcade93448625637ad1b0ac930e5eea199
blin <bennylin@outlook.com>
2015-01-14 16:31:35 +0800

    v3

--- app/src/main/java/com/example/blin/myapplication/NotifySample.java
+++ app/src/main/java/com/example/blin/myapplication/NotifySample.java
@@ -1,19 +1,69 @@
 package com.example.blin.myapplication;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class NotifySample extends ActionBarActivity {
-
+    private static final int FM_NOTIFICATION_ID = 10000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notify_sample);
+        Button InputClick2 = (Button) findViewById(R.id.NotBut);
+        InputClick2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addNotification();
+            }//Onclick Finish
+
+
+        });
+        Button InputClick3 = (Button) findViewById(R.id.CB2);
+        InputClick3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                removeNotification();
+            }
+
+        });
+
+
+    }
+    private void addNotification() {
+
+
+
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle("Notifications Example")
+                        .setContentText("This is a test notification");
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+        // Add as notification
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(FM_NOTIFICATION_ID, builder.build());
     }
 
+    // Remove notification
+    private void removeNotification() {
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.cancel(FM_NOTIFICATION_ID);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
